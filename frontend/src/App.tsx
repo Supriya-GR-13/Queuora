fd40b5d70c5916672e64b757ec17adcada03e55b
Supriya GR <supriyagr618@gmail.com>
2026-08-15 14:11:49 +0530

    Connect frontend to deployed backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://queuora-db.onrender.com";
 
 type User = {
   name: string;
